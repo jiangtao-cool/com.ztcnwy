9711c65d0430aea7f70af823cefdff3520068f2e
Administrator <Administrator@iZ23ar98sg8Z>
2016-06-01 15:36:59 +0800

    11

--- caches/caches_commons/caches_data/model.cache.php
+++ caches/caches_commons/caches_data/model.cache.php
@@ -69,28 +69,5 @@ return array (
     'sort' => '0',
     'type' => '0',
   ),
-  15 => 
-  array (
-    'modelid' => '15',
-    'siteid' => '1',
-    'name' => '视频',
-    'description' => '',
-    'tablename' => 'vedio',
-    'setting' => '',
-    'addtime' => '0',
-    'items' => '0',
-    'enablesearch' => '1',
-    'disabled' => '0',
-    'default_style' => 'default',
-    'category_template' => 'category',
-    'list_template' => 'list',
-    'show_template' => 'show',
-    'js_template' => '',
-    'admin_list_template' => '',
-    'member_add_template' => '',
-    'member_list_template' => '',
-    'sort' => '0',
-    'type' => '0',
-  ),
 );
 ?>
--- caches/caches_template/default/content/category.php
+++ caches/caches_template/default/content/category.php
@@ -6,57 +6,9 @@
 
 
 
-<link rel="stylesheet" href="muban/style/list.css" />
-<style>
-	.img-content {
-		
-	}
-	.img-content>img{
-		width: 15%;
-	}
-	.img-content>div{
-		display: inline-block;
-		width: 77%;
-		margin-left: 4%;
-		vertical-align: top;
-	}
-	.data-list li {
-		display: inline-block;
-		padding: 0;
-		width: 45%;
-		height: 110px;
-	}
-	.data-list li>h3{
-		margin: 10px 0 10px 0;
-	}
-	.data-list a {
-		color: black;
-	}
-	
-	.data-list table{width: 100%;}	
-	.data-img {
-		width: 20%;
-	}
-	.data-img img{
-		width: 100%;
-	}
-	.data-content2 {
-		margin-top: 0;
-		padding: 0;
-		vertical-align: top;
-		width: 75%;
-		
-	}
-	.data-content2>div{
-		padding: 5px;
-		vertical-align: top;
-		height: 100%;
-		display: inline-block;
-		width: 80%;
-	}
-	
-	
-</style>
+<link rel="stylesheet" href="/muban/style/list.css" />
+<link rel="stylesheet" href="/muban/style/category.css" />
+
 <!--left menu-->
 <div class="base">
 	<div class="content">
@@ -83,7 +35,7 @@
 					</h1>
 					
 						
-					<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=66b0411f0395411ac053fbcd08215df4&action=lists&catid=%24id&order=id+DESC&num=4&moreinfo=1&page=%24page\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'lists')) {$pagesize = 4;$page = intval($page) ? intval($page) : 1;if($page<=0){$page=1;}$offset = ($page - 1) * $pagesize;$content_total = $content_tag->count(array('catid'=>$id,'order'=>'id DESC','moreinfo'=>'1','limit'=>$offset.",".$pagesize,'action'=>'lists',));$pages = pages($content_total, $page, $pagesize, $urlrule);$data = $content_tag->lists(array('catid'=>$id,'order'=>'id DESC','moreinfo'=>'1','limit'=>$offset.",".$pagesize,'action'=>'lists',));}?>
+					<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=00deb04f2db7d003ed0083f210afaab3&action=lists&catid=%24id&order=id+DESC&num=3&moreinfo=1&page=%24page\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'lists')) {$pagesize = 3;$page = intval($page) ? intval($page) : 1;if($page<=0){$page=1;}$offset = ($page - 1) * $pagesize;$content_total = $content_tag->count(array('catid'=>$id,'order'=>'id DESC','moreinfo'=>'1','limit'=>$offset.",".$pagesize,'action'=>'lists',));$pages = pages($content_total, $page, $pagesize, $urlrule);$data = $content_tag->lists(array('catid'=>$id,'order'=>'id DESC','moreinfo'=>'1','limit'=>$offset.",".$pagesize,'action'=>'lists',));}?>
 						<ul class="mt20 data-list" >
 							 <?php $n=1;if(is_array($data)) foreach($data AS $r) { ?>
 							 <li > 
@@ -99,9 +51,8 @@
 						 </ul>
 					 <?php if(defined('IN_ADMIN') && !defined('HTML')) {echo '</div>';}?>
 							
-						
 		   					
-					
+					<div id="pages" class="text-c"><?php echo $pages;?></div>
 					
 				</td>
 			</tr>
