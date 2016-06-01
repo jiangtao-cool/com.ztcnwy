9bc650a253ad7907ffde54ea12a4d66b0c0d6513
Administrator <Administrator@iZ23ar98sg8Z>
2016-06-01 09:57:07 +0800

    111

--- caches/caches_template/default/content/category_video.php
+++ caches/caches_template/default/content/category_video.php
@@ -47,31 +47,24 @@
 						<?php echo $current_cat['description'];?>
 					</h1>
 					
-					<table>
-						<tr>
-							<?php $n=1;if(is_array(subcat($id))) foreach(subcat($id) AS $v) { ?> 
-								<td><h2><a href="<?php echo $url;?>&id=<?php echo $v['catid'];?>"><?php echo $v['catname'];?></a></h2></td>
-							<?php $n++;}unset($n); ?>
-						</tr>
+						<?php $n=1;if(is_array(subcat($id))) foreach(subcat($id) AS $v) { ?> 
+							<h2><?php echo $v['catname'];?></h2>
 						
-					</table>
+							<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=8636a70f9711280bfde315f5e325c9a8&action=lists&catid=%24v%5Bcatid%5D&order=id+DESC&num=4&moreinfo=1\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'lists')) {$data = $content_tag->lists(array('catid'=>$v[catid],'order'=>'id DESC','moreinfo'=>'1','limit'=>'4',));}?>
+								<ul class="mt20">
+									 <?php $n=1; if(is_array($data)) foreach($data AS $key => $val) { ?>
+									 <li> 
+									 	<?php echo $val['video_code'];?>
+									 </li>
+									 <?php $n++;}unset($n); ?>
+								 </ul>
+							 <?php if(defined('IN_ADMIN') && !defined('HTML')) {echo '</div>';}?>
+							
+						
+						<?php $n++;}unset($n); ?>
 		   					
 					
-					<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=2ce97bbadd157c53adf9574417b61365&action=lists&catid=%24id&order=id+DESC&num=4&moreinfo=1\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'lists')) {$data = $content_tag->lists(array('catid'=>$id,'order'=>'id DESC','moreinfo'=>'1','limit'=>'4',));}?>
-					 <ul class="mt20">
-						 <?php $n=1; if(is_array($data)) foreach($data AS $key => $val) { ?>
-						 <li> 
-						 	<h3><?php echo $val['title'];?> <em class="right"><?php echo date('Y-m-d',$val[inputtime]);?></em> </h3>
-						 	<div class="img-content mt10">
-						 		<img src="<?php echo $val['thumb'];?>" />
-						 		<div class="text-border">
-						 		 <?php echo $val['content'];?>
-						 		</div>
-						 	</div>
-						 </li>
-						 <?php $n++;}unset($n); ?>
-					 </ul>
-					 <?php if(defined('IN_ADMIN') && !defined('HTML')) {echo '</div>';}?>
+					
 				</td>
 			</tr>
 			
